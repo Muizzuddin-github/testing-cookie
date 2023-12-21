cb17dbb646f9071f0a765a5d1ac444bef247359d
Muizzuddin-github <muizzuddin334@gmail.com>
2023-12-21 16:52:59 +0700

    change url

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import cookieParser from "cookie-parser";
 const app = express();
 app.use(
   cors({
-    origin: "https://front-end-testing-cookie.vercel.app",
+    origin: "http://localhost:5173",
     credentials: true,
   })
 );
