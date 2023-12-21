1b2eebdfa68f12718ac514ef1e6b54d5aec38022
Muizzuddin-github <muizzuddin334@gmail.com>
2023-12-21 16:48:43 +0700

    change methode and set proxy

--- index.js
+++ index.js
@@ -4,24 +4,30 @@ import session from "express-session";
 import cookieParser from "cookie-parser";
 
 const app = express();
-app.use(cors({ origin: "https://front-end-testing-cookie.vercel.app", credentials: true }));
+app.set("trust proxy", 1);
+app.use(
+  cors({
+    origin: "https://front-end-testing-cookie.vercel.app",
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 
-// app.use(
-//   session({
-//     name: "kuy",
-//     secret: "kuykuy",
-//     resave: false,
-//     saveUninitialized: true,
-//     cookie: {
-//       secure: true,
-//       httpOnly: true,
-//       maxAge: 3600000,
-//       sameSite: "strict",
-//     },
-//   })
-// );
+app.use(
+  session({
+    name: "kuy",
+    secret: "kuykuy",
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      secure: true,
+      httpOnly: true,
+      maxAge: 3600000,
+      sameSite: "strict",
+    },
+  })
+);
 
 app.get("/", function (req, res) {
   res.send("berhasil terhubung");
@@ -34,7 +40,7 @@ app.post("/login", function (req, res) {
     return res.status(400).json({ message: "check your email or password" });
   }
 
-  res.cookie("token", "login success");
+  res.session.token = "login berhasil";
 
   res.status(200).json({ message: "login success" });
 });
