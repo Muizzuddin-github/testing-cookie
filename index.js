17838f579ef55fbad16b5572cd22a1388c3bb9d0
Muizzuddin-github <muizzuddin334@gmail.com>
2023-12-21 16:33:25 +0700

    ganti methode

--- index.js
+++ index.js
@@ -1,25 +1,27 @@
 import express from "express";
 import cors from "cors";
 import session from "express-session";
+import cookieParser from "cookie-parser";
 
 const app = express();
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.json());
+app.use(cookieParser());
 
-app.use(
-  session({
-    name: "kuy",
-    secret: "kuykuy",
-    resave: false,
-    saveUninitialized: true,
-    cookie: {
-      secure: true,
-      httpOnly: true,
-      maxAge: 3600000,
-      sameSite: "strict",
-    },
-  })
-);
+// app.use(
+//   session({
+//     name: "kuy",
+//     secret: "kuykuy",
+//     resave: false,
+//     saveUninitialized: true,
+//     cookie: {
+//       secure: true,
+//       httpOnly: true,
+//       maxAge: 3600000,
+//       sameSite: "strict",
+//     },
+//   })
+// );
 
 app.get("/", function (req, res) {
   res.send("berhasil terhubung");
@@ -32,7 +34,7 @@ app.post("/login", function (req, res) {
     return res.status(400).json({ message: "check your email or password" });
   }
 
-  req.session.token = "loginsuccess";
+  res.cookie("token", "login success");
 
   res.status(200).json({ message: "login success" });
 });
